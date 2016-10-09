827ae9297e1abe8144e11f68f6eb7db17d122fd9
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2016-10-09 15:31:50 -0700

    Bind controls with variables

--- Manifest/ViewController.swift
+++ Manifest/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBAction func handleSignUp(_ sender: AnyObject) {
+        
+        
+        FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            print(user, error)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
