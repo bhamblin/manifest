e9a6e85c2dedb624c7426ec1aabc468664cfa4b0
Bradley Hamblin <bhamblin@bradleys-macbook-pro.local>
2016-10-09 13:48:31 -0700

    Initial Commit

---
+++ Manifest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manifest
+//
+//  Created by Bradley Hamblin on 10/9/16.
+//  Copyright © 2016 Bradley Hamblin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
